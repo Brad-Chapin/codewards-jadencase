129368ca7c639cc13696fdc036ad6ff8539164b6
Brad Chapin <paledoor@yahoo.com>
2018-01-22 09:37:24 -0800

    working function

---
+++ jadencase.js
@@ -0,0 +1,11 @@
+function jadencase (str) {
+  let words = str.split(' ');
+  for (let i = 0; i < words.length; i++){
+    let letters = words[i].split('');
+    letters[0] = letters[0].toUpperCase();
+    words[i] = letters.join('');
+  }
+  return words.join(' ');
+}
+
+console.log(jadencase("this is only a test"));
